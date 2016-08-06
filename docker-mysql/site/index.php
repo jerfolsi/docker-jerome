d85c4cf236e4214c51438e06c3338ea97b63620f
jerome <jerome@imac-de-jerome.home>
2016-08-06 22:22:42 +0200

    add index.php

---
+++ docker-mysql/site/index.php
@@ -0,0 +1,14 @@
+<?php
+
+$servername = "172.17.0.2";
+$username = "root";
+$password = "root";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
